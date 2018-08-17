f5cb6a3d311ea1f5928784157237564ed8fdfd7f
Petr Petukhov <kotov0991@gmail.com>
2018-08-17 22:23:17 +0300

    html layout ready

--- app/js/common.js
+++ app/js/common.js
@@ -57,6 +57,7 @@ $(function() {
 		inputIndex = $input.closest('.checkbox').index();
 
 		$(".offer-item").hide().eq(inputIndex).fadeIn("normal");
+		$(".offer-img-descr-text").hide().eq(inputIndex).fadeIn("normal");
 
 		switch(inputIndex) {
 			case 0: 
@@ -64,23 +65,197 @@ $(function() {
 			case 2: 
 				$('.offer-list-1').show();
 				$('.offer-price-val-1').show();
+				$('.model-type-1').show();
 				$('.offer-list-2').hide();
 				$('.offer-price-val-2').hide();
+				$('.model-type-2').hide();
 			break;
 			case 3: 
 			case 4: 
 				$('.offer-list-2').show();
 				$('.offer-price-val-2').show();
+				$('.model-type-2').show();
+
 				$('.offer-list-1').hide();
 				$('.offer-price-val-1').hide();
+				$('.model-type-1').hide();
+
 			break;
 		}
 
-		// $("#hiddenColorInput").val($input.data("item"));
+		$("#hiddenColorInput").val($input.data("item"));
+
+	});
+
+});
+
+
+
+$(function() {
+
+	//плавный скролл по кнопке "подробнее" 
+	$(".bottomMore").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top - 70}, 1000);
 	});
 
 
 
+	//плавный скролл по меню и пунктам меню
+	$(".nav").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top - 70}, 1000);
+			$('.navWrap').removeAttr('style');
+	});
+
+
+	//кнопка "обратная связь"
+	function feedback(){
+		$('.feedbackFormBtn-js').on("click", function(e){
+				e.preventDefault();
+			if($(window).width() > 767){
+				$('.feedbackForm .feedbackFormBtn-js').removeClass("plTxt");
+				$('.feedbackForm .feedbackFormBtn-js .feedbackFormBtn__arrow').toggleClass('arrowToggle');
+				if($(".feedbackForm__hiddenForm").is(":visible")){
+					$(".feedbackForm__hiddenForm").slideUp();
+					$('.feedbackFormBtn-js .feedbackFormBtn__text').text("обратная связь");
+				}else {
+					$(".feedbackForm__hiddenForm").slideDown();
+					$('.feedbackForm .feedbackFormBtn-js .feedbackFormBtn__text').text("свернуть");
+					$('.feedbackForm .feedbackFormBtn-js').addClass("plTxt");
+
+
+				}
+			}
+		});
+	}
+
+	$(window).on("load resize", feedback());
+
+
+
+	//мобильное меню
+
+	$('.menuMobile-js').on('click', function(e) {
+		e.preventDefault();
+		$('.navWrap').slideToggle(300, function(){
+			
+			if($(this).css('display')==='none'){
+				$(this).removeAttr('style');
+				}else{
+					$('.feedbackForm').slideUp(100);
+				}
+		});
+		
+	});
+
+	function navWrapHidden(){
+		if($(window).width() > 767){
+					$('.navWrap').removeAttr('style');
+				}	
+	}
+
+$(window).on("resize", navWrapHidden);
+
+
+
+
+	//мобильная форма обратной связи
+	$('.feedbackMobile-js').on('click', function(e) {
+		e.preventDefault();
+		$('.feedbackForm').slideToggle(300, function(){
+			
+			if($(this).css('display')==='none'){
+				$(this).removeAttr('style');
+				}else{
+					$('.navWrap').slideUp(100);
+				}
+		});
+		
+	});
+
+
+//валидация формы
+
+$('.feedbackFormBlock input').on('input', function(){
+	var th = $(this);
+	var val = $(this).val();
+	if(val.length > 4 ){
+			console.log('ok222');
+			$(this).siblings(".formValid").find('.formCheck').css('display', 'block');
+		}else{
+			$(this).siblings(".formValid").find('.formCheck').css('display', 'none');
+		}
+});
+
+
+$('.popupBuyFormBlock input').on('input', function(){
+	var th = $(this);
+	var val = $(this).val();
+	if(val.length > 4 ){
+		console.log(val);
+			console.log('ok');
+			$(this).siblings(".formValid").find('.formCheck').css('display', 'block');
+		}else{
+			$(this).siblings(".formValid").find('.formCheck').css('display', 'none');
+		}
+});
+
+
+
+
+
+
+
+
+/*открытие и закрытие модального окна с формой*/
+
+	//open popup
+	$('.offer-btn').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
 	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+			if(event.which=='27'){
+				$('.cd-popup').removeClass('is-visible');
+			}
+		});
+
+
+/*открытие и закрытие модального окна с формой*/
+
+	//open popup
+	$('.cd-popup-trigger2').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup2').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup2').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup2') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+			if(event.which=='27'){
+				$('.cd-popup2').removeClass('is-visible');
+			}
+		});
+
 
 });
